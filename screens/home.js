5856d065f53ad84b52c333b15bd12a0db15e1b81
AleLimia <nekro.lin@gmail.com>
2020-09-28 22:30:43 -0300

    Adding fonts

---
+++ screens/home.js
@@ -0,0 +1,39 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { AppLoading } from 'expo';
+import { useFonts, Montserrat_400Regular, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
+
+export default function App() {
+	let [fontsLoaded] = useFonts({
+	  Montserrat_400Regular,
+	  Montserrat_700Bold,
+	});
+
+    if (!fontsLoaded) {
+		return <AppLoading />;
+	} 
+	else {
+		return (
+		<View style={styles.container}>
+			<Text style={styles.bold}>Home</Text>
+			<StatusBar style="auto" />
+		</View>
+		)
+	}
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	regular: {
+		fontFamily: 'Montserrat_400Regular'
+	},
+	bold: {
+		fontFamily: 'Montserrat_700Bold'
+	}
+})
---
+++ styles/generalStyles.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+export const generalStyles = StyleSheet.create({
+	regular: {
+		fontFamily: 'Montserrat_400Regular'
+	},
+	bold: {
+		fontFamily: 'Montserrat_700Bold'
+	}
+})
