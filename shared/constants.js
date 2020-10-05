a990a57a7cdc57e447414f4dadda3f3f0a8801ce
AleLimia <nekro.lin@gmail.com>
2020-10-05 11:54:57 -0300

    Actualizacion ngrok

--- shared/constants.js
+++ shared/constants.js
@@ -1,3 +1,3 @@
 export const RUTA_API = 'https://a3490a9f1d0d.ngrok.io' 
-// DESDE 2/10/2020 a las 15:40h
-// HASTA 2/10/2020 a las 21:40h
+// DESDE 5/10/2020 a las 12:00h
+// HASTA 5/10/2020 a las 20:00h
